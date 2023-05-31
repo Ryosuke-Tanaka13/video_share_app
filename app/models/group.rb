129fc6e40d27cacc2fcef8e511ca82278c6edade
Leeo-pro <mineva.aug@gmail.com>
2023-05-31 06:03:02 +0000

    GroupのMVC設定

--- app/models/group.rb
+++ app/models/group.rb
@@ -2,4 +2,7 @@ class Group < ApplicationRecord
   belongs_to :organization
   has_many :viewer_groups
   has_many :viewers, through: :viewer_groups,dependent: :destroy
+  
+  before_create -> { self.uuid = SecureRandom.uuid }
+  validates :name,  presence: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -51,6 +51,7 @@ ActiveRecord::Schema.define(version: 2023_03_12_055530) do
   create_table "groups", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "name"
     t.integer "organization_id"
+    t.string "uuid"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
