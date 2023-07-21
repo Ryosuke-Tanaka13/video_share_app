cce6f8ebff670639e011d27b9cd9da77c58d4b13
atsutakahiro <takahiro619z@icloud.com>
2023-07-21 13:09:59 +0900

    show.js修正

--- app/javascript/packs/videos/show.js
+++ app/javascript/packs/videos/show.js
@@ -1,15 +1,15 @@
-document.addEventListener('DOMContentLoaded', (event) => {
-  let button = document.getElementById("toggle-comments-button");
-  let commentsArea = document.getElementById("comments_area");
+document.addEventListener('turbolinks:load', function() {
+  let button = $("#toggle-comments-button");
+  let commentsArea = $("#comments_area");
 
-  if (button && commentsArea) {
-    button.addEventListener("click", function() {
-      if (commentsArea.style.display === "none") {
-        commentsArea.style.display = "block";
-        button.textContent = "コメントを非表示にする";
+  if (button.length && commentsArea.length) {
+    button.on("click", function() {
+      if (commentsArea.css("display") === "none") {
+        commentsArea.show();
+        button.text("コメントを非表示にする");
       } else {
-        commentsArea.style.display = "none";
-        button.textContent = "コメントを表示する";
+        commentsArea.hide();
+        button.text("コメントを表示する");
       }
     });
   }
