65233215f19208c23e9631f9f3c3b697e5eb2de0
atsutakahiro <takahiro619z@icloud.com>
2024-01-18 12:50:21 +0900

    video_comments_spec.rb　修正

--- spec/system/videos/video_comments_spec.rb
+++ spec/system/videos/video_comments_spec.rb
@@ -1,24 +1,24 @@
 # spec/features/video_comments_spec.rb
 require 'rails_helper'
 
-RSpec.xdescribe "ビデオコメント", type: :feature, js: true do
-  let!(:video_it) { create(:video_it) } 
+RSpec.xdescribe 'ビデオコメント', type: :feature, js: true do
+  let!(:video_it) { create(:video_it) }
 
-  it "コメントの表示・非表示を切り替えられる" do
+  it 'コメントの表示・非表示を切り替えられる' do
     visit video_path(video_it)
 
     # 初期状態の確認
-    expect(page).not_to have_css("#comments_area")
-    expect(page).to have_button("コメントを表示する")
+    expect(page).not_to have_css('#comments_area')
+    expect(page).to have_button('コメントを表示する')
 
     # コメント表示
-    click_button "コメントを表示する"
-    expect(page).to have_css("#comments_area")
-    expect(page).to have_button("コメントを非表示にする")
+    click_button 'コメントを表示する'
+    expect(page).to have_css('#comments_area')
+    expect(page).to have_button('コメントを非表示にする')
 
     # コメント非表示
-    click_button "コメントを非表示にする"
-    expect(page).not_to have_css("#comments_area")
-    expect(page).to have_button("コメントを表示する")
+    click_button 'コメントを非表示にする'
+    expect(page).not_to have_css('#comments_area')
+    expect(page).to have_button('コメントを表示する')
   end
 end
