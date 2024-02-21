bfb6e197203b552d10467cade4b35597e22d66da
atsutakahiro <takahiro619z@icloud.com>
2024-02-21 21:10:06 +0900

    限定公開テスト完了

--- spec/requests/videos/videos_show_spec.rb
+++ spec/requests/videos/videos_show_spec.rb
@@ -1,21 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe 'Videos', type: :request do
-  let(:organization) { create(:organization) }
-  let(:user) { create(:user, organization: organization) }
-  let(:group) { create(:group, organization: organization) }
-  let(:viewer) { create(:viewer) }
+  let(:organization) { create(:test_organization) }
+  let(:user) { create(:test_user, confirmed_at: Time.now) }
+  let(:group) { create(:group) }
+  let(:viewer) { create(:viewer, confirmed_at: Time.now) }
   let!(:viewer_group) { create(:viewer_group, viewer: viewer, group: group) }
-  let(:video) { create(:video_it, user: user, groups: [group], range: true) } # range: true は限定公開
-
+  let!(:organization_viewer) { create(:organization_viewer, viewer: viewer, organization: organization) } 
+  let!(:video) { create(:test_video, user: user, organization: organization, groups: [group], range: true) } # range: true は限定公開
   describe 'GET /videos' do
     context '視聴者がログインしている場合' do
       it 'ビデオが表示される' do
         # 視聴者としてログイン
         sign_in viewer
-        # ビデオ一覧ページにアクセス
-        get videos_path
-        follow_redirect!
+        get videos_path(organization_id: organization.id) 
         expect(response).to have_http_status(:ok)
         # レスポンスボディにビデオのタイトルが含まれていることを確認
         expect(response.body).to include(video.title)
