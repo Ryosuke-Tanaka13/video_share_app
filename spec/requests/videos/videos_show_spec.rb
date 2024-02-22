648679a8665505bf47f07f003c2dddfe31dbd0f4
atsutakahiro <takahiro619z@icloud.com>
2024-02-22 14:58:07 +0900

    テストコード修正

--- spec/requests/videos/videos_show_spec.rb
+++ spec/requests/videos/videos_show_spec.rb
@@ -7,16 +7,49 @@ RSpec.describe 'Videos', type: :request do
   let(:viewer) { create(:viewer, confirmed_at: Time.now) }
   let!(:viewer_group) { create(:viewer_group, viewer: viewer, group: group) }
   let!(:organization_viewer) { create(:organization_viewer, viewer: viewer, organization: organization) } 
-  let!(:video) { create(:test_video, user: user, organization: organization, groups: [group], range: true) } # range: true は限定公開
+  let!(:video) { create(:test_video1, user: user, organization: organization, groups: [group], range: true) } # range: true は限定公開
+  let(:other_viewer) { create(:another_viewer, confirmed_at: Time.now) }
+  let!(:other_organization_viewer) { create(:organization_viewer2, viewer: other_viewer, organization: organization) } 
+  let!(:public_video) { create(:test_video2, user: user, organization: organization, range: false) } # range: false は一般公開
+
   describe 'GET /videos' do
-    context '視聴者がログインしている場合' do
-      it 'ビデオが表示される' do
+    context '視聴者がログインし、限定公開ビデオの閲覧権限がある場合' do
+      it '限定公開ビデオが表示される' do
         # 視聴者としてログイン
         sign_in viewer
         get videos_path(organization_id: organization.id) 
         expect(response).to have_http_status(:ok)
-        # レスポンスボディにビデオのタイトルが含まれていることを確認
+        # レスポンスボディに限定公開ビデオのタイトルが含まれていることを確認
         expect(response.body).to include(video.title)
+      end
+
+      it '一般公開ビデオも表示される' do
+        # 視聴者としてログイン
+        sign_in viewer
+        get videos_path(organization_id: organization.id) 
+        expect(response).to have_http_status(:ok)
+        # レスポンスボディに一般公開ビデオのタイトルも含まれていることを確認
+        expect(response.body).to include(public_video.title)
+      end
+    end
+
+    context '視聴者がログインし、限定公開ビデオの閲覧権限がない場合' do
+      it '一般公開ビデオのみが表示される' do
+        # 別の視聴者としてログイン
+        sign_in other_viewer
+        get videos_path(organization_id: organization.id) 
+        expect(response).to have_http_status(:ok)
+        # レスポンスボディに一般公開ビデオのタイトルが含まれていることを確認
+        expect(response.body).to include(public_video.title)
+      end
+
+      it '限定公開ビデオは表示されない' do
+        # 別の視聴者としてログイン
+        sign_in other_viewer
+        get videos_path(organization_id: organization.id) 
+        expect(response).to have_http_status(:ok)
+        # レスポンスボディに限定公開ビデオのタイトルが含まれていないことを確認
+        expect(response.body).not_to include(video.title)
       end
     end
   end
