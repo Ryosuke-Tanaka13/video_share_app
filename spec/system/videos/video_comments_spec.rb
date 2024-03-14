a365a9fc929bda21808075f7c389da2bec338479
atsutakahiro <takahiro619z@icloud.com>
2024-03-14 19:01:51 +0900

    edit video_comments_spec.rb

--- spec/system/videos/video_comments_spec.rb
+++ spec/system/videos/video_comments_spec.rb
@@ -1,11 +1,22 @@
 require 'rails_helper'
 
-RSpec.xdescribe 'ビデオコメント', type: :feature, js: true do
-  let!(:video_it) { create(:video_it) }
+RSpec.describe 'ビデオコメントの表示・非表示', type: :feature, js: true do
+  let!(:organization) { create(:organization) }
+  let!(:viewer) { create(:viewer, confirmed_at: Time.now, organizations: [organization]) }
+  let!(:video_it) { create(:video_it, organization: organization) }
+
+  before do
+    # ログインページにアクセスしてログインする
+    visit new_viewer_session_path
+    fill_in 'メールアドレス', with: viewer.email
+    fill_in 'パスワード', with: viewer.password
+    click_button 'ログイン'
+
+    # 動画詳細ページにアクセス
+    visit video_path(video_it)
+  end
 
   it 'コメントの表示・非表示を切り替えられる' do
-    visit video_path(video_it)
-
     # 初期状態の確認
     expect(page).not_to have_css('#comments_area')
     expect(page).to have_button('コメントを表示する')
