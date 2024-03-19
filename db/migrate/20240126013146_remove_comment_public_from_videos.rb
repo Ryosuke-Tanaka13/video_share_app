b588407c7b813bb5ee7aad24bd6a627efa35374f
atsutakahiro <takahiro619z@icloud.com>
2024-03-19 10:16:34 +0900

    delete file

--- db/migrate/20240126013146_remove_comment_public_from_videos.rb
+++
@@ -1,5 +0,0 @@
-class RemoveCommentPublicFromVideos < ActiveRecord::Migration[6.1]
-  def change
-    remove_column :videos, :comment_public, :boolean if column_exists? :videos, :comment_public
-  end
-end
