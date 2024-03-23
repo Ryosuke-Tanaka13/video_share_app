3239bdcad36313ffdde3a93b2340d5e54823b805
atsutakahiro <takahiro619z@icloud.com>
2024-03-23 20:33:43 +0900

    group_spec.rb

--- spec/models/group_spec.rb
+++ spec/models/group_spec.rb
@@ -1,5 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe Group, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:organization) { Organization.create(name: 'organization_name') }
+  let(:group) { Group.new(name: 'group_name', organization: organization) }
+  
+  describe 'バリデーション' do
+    context '名前が設定されている場合' do
+      it 'バリデーションが通る' do
+        expect(group.valid?).to eq(true)
+      end
+    end
+
+    context '名前が設定されていない場合' do
+      it 'バリデーションが通らない' do
+        group.name = nil
+        expect(group.valid?).to eq(false)
+      end
+    end
+  end
 end
