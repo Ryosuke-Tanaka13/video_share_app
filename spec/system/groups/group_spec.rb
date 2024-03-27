0fa590e83199a508049628b5d7503e0cc4085c9d
takahiro619 <takahiro619z@gmail.com>
2024-03-27 20:44:42 +0900

    edit it

--- spec/system/groups/group_spec.rb
+++ spec/system/groups/group_spec.rb
@@ -35,55 +35,7 @@ RSpec.describe 'グループ新規登録', type: :system do
     organization_viewer2
     organization_viewer3
   end
-  it 'グループが正しく作成され、uuidが存在する' do
-    @organization = Organization.create(name: 'Test Organization') # 既存のOrganizationを作成
-    @group = Group.new(name: 'Old Group Name', organization: @organization) # 既存のグループを作成
-    if @group.valid?
-      @group.save
-      expect(@group.uuid).not_to be_nil
-    else
-      puts @group.errors.full_messages
-    end
-  end
-end
-require 'rails_helper'
-
-RSpec.describe 'グループ新規登録', type: :system do
-  let(:system_admin) { create(:system_admin, confirmed_at: Time.now) }
-
-  let(:organization) { create(:organization) }
-  let(:user_owner) { create(:user_owner, confirmed_at: Time.now) }
-  let(:user_staff) { create(:user_staff, confirmed_at: Time.now) }
-  let(:viewer) { create(:viewer, confirmed_at: Time.now) }
-  let(:viewer1) { create(:viewer1, confirmed_at: Time.now) }
-
-  let(:another_organization) { create(:another_organization) }
-  let(:another_user_owner) { create(:another_user_owner, confirmed_at: Time.now) }
-  let(:another_user_staff) { create(:another_user_staff, confirmed_at: Time.now) }
-  let(:another_viewer) { create(:another_viewer, confirmed_at: Time.now) }
-
-  let(:organization_viewer) { create(:organization_viewer) }
-  let(:organization_viewer1) { create(:organization_viewer1) }
-  let(:organization_viewer2) { create(:organization_viewer2) }
-  let(:organization_viewer3) { create(:organization_viewer3) }
-
-  before(:each) do
-    system_admin
-    organization
-    user_owner
-    user_staff
-    viewer
-    viewer1
-    another_organization
-    another_user_owner
-    another_user_staff
-    another_viewer
-    organization_viewer
-    organization_viewer1
-    organization_viewer2
-    organization_viewer3
-  end
-
+  
   describe 'グループの新規登録' do
     before(:each) do
       login(user_owner)
