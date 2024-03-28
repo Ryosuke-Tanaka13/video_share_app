508ce57aa3f758ca7d49483c479a3b00b74d7281
takahiro619 <takahiro619z@gmail.com>
2024-03-28 14:30:49 +0900

    全体を変更。group_spec

--- spec/system/groups/group_spec.rb
+++ spec/system/groups/group_spec.rb
@@ -78,5 +78,45 @@ RSpec.describe 'グループ新規登録', type: :system do
       # 新規登録ページへ戻されることを確認する
       expect(page).to have_current_path('/groups')
     end
+
+    describe 'グループの編集' do
+      let(:group) { create(:group, name: 'New Group Name') }
+  
+      before(:each) do
+        login(user_owner)
+        group
+        visit groups_path
+      end
+  
+      it '正しい情報を入力すればグループの編集ができて一覧画面に移動する' do
+        expect(page).to have_content('New Group Name')
+        # 編集ページへ移動する
+        find_link('編集', href: edit_group_path(group.uuid)).click
+        expect(page).to have_current_path(edit_group_path(group.uuid))
+  
+        # グループ名を編集する
+        fill_in 'group[name]', with: 'Edit Group Name'
+        find('input[name="commit"]').click
+  
+        # 一覧ページへ遷移したことを確認し、新しいグループ名が表示されていることを確認する
+        expect(page).to have_current_path groups_path, ignore_query: true
+        expect(page).to have_content('Edit Group Name')
+      end
+  
+      it 'グループ名を空で更新しようとするとエラーメッセージが表示される' do
+        expect(page).to have_content('New Group Name')
+        # 編集ページへ移動する
+        find_link('編集', href: edit_group_path(group.uuid)).click
+        expect(page).to have_current_path(edit_group_path(group.uuid))
+  
+        # グループ名を空にして更新する
+        fill_in 'group[name]', with: ''
+        find('input[name="commit"]').click
+  
+        # 編集ページに留まり、エラーメッセージが表示されることを確認する
+        expect(page).to have_current_path(group_path(group.uuid))
+        expect(page).to have_content('エラー')
+      end
+    end
   end
 end
