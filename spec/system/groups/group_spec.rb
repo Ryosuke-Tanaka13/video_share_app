af07d6b9e005a16ecc2e06c2fa030c969b6fffe0
takahiro619 <takahiro619z@gmail.com>
2024-03-28 12:33:30 +0900

    テスト内での変数スコープを修正
    - `before(:each)`ブロック内で作成した`test_user_owner`をインスタンス変数に変更
    - `it`ブロック内で`@test_user_owner`を参照できるように修正

--- spec/system/groups/group_spec.rb
+++ spec/system/groups/group_spec.rb
@@ -42,15 +42,15 @@ RSpec.describe 'グループ新規登録', type: :system do
     before(:each) do
       organization = Organization.create(name: 'Test Organization') # 既存のOrganizationを作成
       @group = Group.create(name: 'Old Group Name', organization: organization) # 既存のグループを作成
-      test_user_owner = create(:test_user_owner, confirmed_at: Time.now) # test_user_ownerを作成
-      login(test_user_owner)
-      current_user(test_user_owner)
+      @test_user_owner = create(:test_user_owner, confirmed_at: Time.now) # test_user_ownerを作成
+      login(@test_user_owner)
+      current_user(@test_user_owner)
       visit edit_group_path(@group) # 編集ページに移動
     end
 
     it 'グループが正しく作成されている' do
       expect(@group).to be_present
-      expect(@group.organization_id).to eq(test_user_owner.organization_id)
+      expect(@group.organization_id).to eq(@test_user_owner.organization_id)
     end
   end
 end
