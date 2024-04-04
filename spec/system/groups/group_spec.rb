1557b165668c7ab01ef7c92e01c78407937e7ae1
takahiro619 <takahiro619z@gmail.com>
2024-04-05 07:38:13 +0900

    before(:each) do 修正

--- spec/system/groups/group_spec.rb
+++ spec/system/groups/group_spec.rb
@@ -5,6 +5,12 @@ RSpec.describe 'グループ新規登録', type: :system do
   let(:user_staff) { create(:user_staff, confirmed_at: Time.now) }
   let(:user_owner) { create(:user_owner, confirmed_at: Time.now) }
 
+  before(:each) do
+    organization
+    user_owner
+    user_staff
+  end
+  
   describe 'グループの新規登録' do
     before(:each) do
       sign_in(user_owner)
