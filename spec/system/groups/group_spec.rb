340e183aa4fef05a8f33135bfab64bde05b459a8
takahiro619 <takahiro619z@gmail.com>
2024-04-09 22:51:21 +0900

    視聴者選択のテストも追加

--- spec/system/groups/group_spec.rb
+++ spec/system/groups/group_spec.rb
@@ -27,10 +27,16 @@ RSpec.describe 'グループ新規登録', type: :system do
     end
   
     describe '確認' do
-      it 'current_user.organization.organization_viewers.viewer.name を出力する' do
-        current_user.organization.organization_viewers.each do |organization_viewer|
-          puts organization_viewer.viewer.name
-        end
+      it '正しい情報を入力すればグループ新規登録ができて一覧画面に移動する' do
+        expect(page).to have_content('視聴グループ　新規作成画面へ')
+        visit new_group_path
+        fill_in 'group[name]', with: 'Group Name'
+        # フォームの選択肢にorganization_viewer.viewer.name, organization_viewer2.viewer.nameが表示されていることを確認する。
+        expect(page).to have_select('viewer-select', options: [organization_viewer.viewer.name, organization_viewer2.viewer.name])
+        # 確認後、organization_viewer.viewer.nameを選択する。
+        select organization_viewer.viewer.name, from: 'viewer-select'
+        expect { find('input[name="commit"]').click }.to change(Group, :count).by(1)
+        expect(page).to have_current_path groups_path, ignore_query: true
       end
     end
   end
