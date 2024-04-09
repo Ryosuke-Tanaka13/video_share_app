6fbf7b6f8e4d22355ad37154b3942e5ee677b8bf
takahiro619 <takahiro619z@gmail.com>
2024-04-09 15:32:17 +0900

    organization_viewer.viewer.name を出力する

--- spec/system/groups/group_spec.rb
+++ spec/system/groups/group_spec.rb
@@ -4,7 +4,9 @@ RSpec.describe 'グループ新規登録', type: :system do
   let(:organization) { create(:organization) }
   let(:user_staff) { create(:user_staff, confirmed_at: Time.now) }
   let(:user_owner) { create(:user_owner, confirmed_at: Time.now) }
-
+  let(:viewer) { create(:viewer, organization: organization) }
+  let(:organization_viewer) { create(:organization_viewer) }
+  let(:organization_viewer2) { create(:organization_viewer2) }
   before(:each) do
     organization
     user_owner
@@ -17,87 +19,10 @@ RSpec.describe 'グループ新規登録', type: :system do
       visit groups_path
     end
 
-    it '正しい情報を入力すればグループ新規登録ができて一覧画面に移動する' do
-      expect(page).to have_content('視聴グループ　新規作成画面へ')
-      visit new_group_path
-      fill_in 'group[name]', with: 'Group Name'
-      expect { find('input[name="commit"]').click }.to change(Group, :count).by(1)
-      expect(page).to have_current_path groups_path, ignore_query: true
-    end
-
-    it '誤った情報ではグループ新規登録ができずに新規登録ページへ戻ってくる' do
-      expect(page).to have_content('視聴グループ　新規作成画面へ')
-      visit new_group_path
-      fill_in 'group[name]', with: ''
-      expect { find('input[name="commit"]').click }.to change(Group, :count).by(0)
-      expect(page).to have_current_path('/groups')
-    end
-  end
-
-  describe 'グループの編集' do
-    before(:each) do
-      sign_in(user_owner)
-      # 新規登録ページに遷移
-      visit new_group_path
-      fill_in 'group[name]', with: 'New Group Name'
-      find('input[name="commit"]').click
-      visit groups_path
-    end
-
-    it '正しい情報を入力すればグループの編集ができて一覧画面に移動する' do
-      expect(page).to have_content('New Group Name')
-      find_link('編集', href: edit_group_path(Group.find_by(name: 'New Group Name').uuid)).click
-      fill_in 'group[name]', with: 'Edited Group Name'
-      find('input[name="commit"]').click
-      expect(page).to have_current_path groups_path, ignore_query: true
-      expect(page).to have_content('Edited Group Name')
-    end
-
-    it 'グループ名を空で更新しようとするとエラーメッセージが表示される' do
-      group = Group.find_by(name: 'New Group Name')
-      visit edit_group_path(group.uuid)
-      fill_in 'group[name]', with: ''
-      find('input[name="commit"]').click
-      expect(page).to have_current_path(group_path(group.uuid))
-      expect(page).to have_content('視聴グループ名を入力してください')
-    end
-  end
-
-  # グループの削除テスト
-  describe 'グループの削除' do
-    let!(:group) { create(:group, organization_id: user_staff.organization_id) }
-
-    context '投稿者でログイン' do
-      before(:each) do
-        sign_in(user_staff)
-        visit groups_path
-      end
-
-      it '視聴グループの削除に失敗する' do
-        expect(page).to have_content(group.name)
-        find_link('削除', href: group_path(group.uuid)).click
-        page.driver.browser.switch_to.alert.accept
-        expect(page).to have_content('権限がありません')
-        expect(page).to have_current_path(groups_path, ignore_query: true)
-        expect(Group.count).to eq 1
-      end
-    end
-
-    context 'オーナーでログイン' do
-      let!(:group) { create(:group, organization_id: user_owner.organization_id) }
-
-      before(:each) do
-        sign_in(user_owner)
-        visit groups_path
-      end
-
-      it '視聴グループの削除に成功する' do
-        expect(page).to have_content(group.name)
-        find_link('削除', href: group_path(group.uuid)).click
-        page.driver.browser.switch_to.alert.accept
-        expect(page).to have_content('グループを削除しました')
-        expect(page).to have_current_path(groups_path, ignore_query: true)
-        expect(Group.count).to eq 0
+    describe '確認' do
+      it 'organization_viewer.viewer.name を出力する' do
+        organization_viewer = create(:organization_viewer)
+        puts organization_viewer.viewer.name
       end
     end
   end
