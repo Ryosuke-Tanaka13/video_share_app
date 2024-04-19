c17f6b358c0245ca5562ad206aa476284e4c1cd2
takahiro619 <takahiro619z@gmail.com>
2024-04-19 15:07:18 +0900

    動画が作成されるように video_searches.rb 修正

--- spec/factories/video_searches.rb
+++ spec/factories/video_searches.rb
@@ -9,12 +9,16 @@ FactoryBot.define do
     popup_after_video { false }
     organization_id { 1 }
     user_id { 1 }
+    
+    after(:build) do |video|
+      video.video.attach(io: File.open('spec/fixtures/files/video_jan_public_owner.mp4'), filename: 'video_jan_public_owner.mp4', content_type: 'video/mp4')
+    end
   end
 
   factory :invalid_video_jan_public_owner, class: 'Video' do
     title { 'テスト動画1月（論理削除済み）' }
     open_period { 'Tue, 31 Jan 2023 23:59:00.000000000 JST +09:00' }
-    range { true }
+    range { true } 
     comment_public { false }
     login_set { false }
     popup_before_video { false }
@@ -22,6 +26,10 @@ FactoryBot.define do
     is_valid { false }
     organization_id { 1 }
     user_id { 1 }
+
+    after(:build) do |video|
+      video.video.attach(io: File.open('spec/fixtures/files/invalid_video_jan_public_owner.mp4'), filename: 'invalid_video_jan_public_owner.mp4', content_type: 'video/mp4')
+    end
   end
 
   factory :video_feb_private_owner, class: 'Video' do
@@ -34,6 +42,10 @@ FactoryBot.define do
     popup_after_video { false }
     organization_id { 1 }
     user_id { 1 }
+
+    after(:build) do |video_sample|
+      video_sample.video.attach(io: File.open('spec/fixtures/files/flower.mp4'), filename: 'flower.mp4', content_type: 'video/mp4')
+    end
   end
 
   factory :video_mar_public_staff, class: 'Video' do
@@ -46,6 +58,10 @@ FactoryBot.define do
     popup_after_video { false }
     organization_id { 1 }
     user_id { 3 }
+
+    after(:build) do |video|
+      video.video.attach(io: File.open('spec/fixtures/files/video_mar_public_staff.mp4'), filename: 'video_mar_public_staff.mp4', content_type: 'video/mp4')
+    end
   end
 
   factory :video_apr_private_staff, class: 'Video' do
@@ -58,6 +74,10 @@ FactoryBot.define do
     popup_after_video { false }
     organization_id { 1 }
     user_id { 3 }
+
+    after(:build) do |video|
+      video.video.attach(io: File.open('spec/fixtures/files/video_apr_private_staff.mp4'), filename: 'video_apr_private_staff.mp4', content_type: 'video/mp4')
+    end
   end
 
   factory :video_may_public_staff1, class: 'Video' do
@@ -70,6 +90,10 @@ FactoryBot.define do
     popup_after_video { false }
     organization_id { 1 }
     user_id { 4 }
+
+    after(:build) do |video|
+      video.video.attach(io: File.open('spec/fixtures/files/video_may_public_staff1.mp4'), filename: 'video_may_public_staff1.mp4', content_type: 'video/mp4')
+    end
   end
 
   factory :another_video_jan_public_another_user_owner, class: 'Video' do
@@ -82,6 +106,10 @@ FactoryBot.define do
     popup_after_video { false }
     organization_id { 2 }
     user_id { 2 }
+
+    after(:build) do |video|
+      video.video.attach(io: File.open('spec/fixtures/files/another_video_jan_public_another_user_owner.mp4'), filename: 'another_video_jan_public_another_user_owner.mp4', content_type: 'video/mp4')
+    end
   end
 
   factory :another_video_feb_private_another_user_staff, class: 'Video' do
@@ -94,5 +122,9 @@ FactoryBot.define do
     popup_after_video { false }
     organization_id { 2 }
     user_id { 5 }
+
+    after(:build) do |video|
+      video.video.attach(io: File.open('spec/fixtures/files/another_video_feb_private_another_user_staff.mp4'), filename: 'another_video_feb_private_another_user_staff.mp4', content_type: 'video/mp4')
+    end
   end
 end
