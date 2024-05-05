0a6a1bc4f3f848eec7dfd22753098899e786d944
takahiro619 <takahiro619z@gmail.com>
2024-05-05 22:10:00 +0900

    edit_group_path に変更

--- spec/system/groups/group_spec.rb
+++ spec/system/groups/group_spec.rb
@@ -78,7 +78,7 @@ RSpec.describe 'グループ新規登録', type: :system do
         visit edit_group_path(group.uuid)
         fill_in 'group[name]', with: ''
         find('input[name="commit"]').click
-        expect(page).to have_current_path(group_path(group.uuid, organization_id: group.organization_id))
+        expect(page).to have_current_path(edit_group_path(group.uuid, organization_id: group.organization_id))
         expect(page).to have_content('視聴グループ名を入力してください')
       end
     end
@@ -102,7 +102,7 @@ RSpec.describe 'グループ新規登録', type: :system do
         visit edit_group_path(group.uuid)
         fill_in 'group[name]', with: ''
         find('input[name="commit"]').click
-        expect(page).to have_current_path(group_path(group.uuid, organization_id: group.organization_id))
+        expect(page).to have_current_path(edit_group_path(group.uuid, organization_id: group.organization_id))
         expect(page).to have_content('視聴グループ名を入力してください')
       end
     end
