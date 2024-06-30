8f0f0bf14fd5a39902e3d288ce5955ac690f8d2b
kikumon <s.kiku3728@gmail.com>
2024-07-01 03:14:53 +0900

    js関連のエラーを修正。

--- app/javascript/packs/uses.js
+++ app/javascript/packs/uses.js
@@ -1,31 +1,9 @@
+// app/javascript/packs/use.js
 
 import 'bootstrap';
-
 import '../stylesheets/uses/main.scss';
-
 import "@fortawesome/fontawesome-free/js/all";
 
 document.addEventListener("turbolinks:load", () => {
   $('[data-toggle="tooltip"]').tooltip()
-});
-
-
-// Uncomment to copy all static images under ../images to the output folder and reference
-// them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
-// or the `imagePath` JavaScript helper below.
-//
-// const images = require.context('../images', true)
-// const imagePath = (name) => images(name, true)
-
-document.addEventListener('DOMContentLoaded', function() {
-  function controlSubmit() {
-    var agreeTermsCheckbox = document.getElementById("agreeTerms");
-    var submitButton = document.querySelector("[type='submit']");
-    submitButton.disabled = !agreeTermsCheckbox.checked;
-  }
-
-  controlSubmit();
-
-  // チェックボックスの状態変更を監視し、変更があった場合にボタンの状態を更新
-  document.getElementById("agreeTerms").addEventListener('change', controlSubmit);
 });
