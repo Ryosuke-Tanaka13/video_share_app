573f8e1ae82e7ee1d935a12e3801550754ed43c4
takahiro619 <takahiro619z@gmail.com>
2024-07-14 14:38:55 +0900

    rubocop エラー修正

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -21,9 +21,9 @@ class GroupsController < ApplicationController
   def new
     @group = Group.new
     @viewers = Viewer.joins(:organization_viewers)
-                     .where(organization_viewers: { organization_id: current_user.organization_id })
-                     .where(is_valid: true)
-  end
+                      .where(organization_viewers: { organization_id: current_user.organization_id })
+                      .where(is_valid: true)
+    end
 
   def create
     @group = Group.new(group_params)
