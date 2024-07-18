ebc4df27be56c062f80174265674c44891078d4d
takahiro619 <takahiro619z@gmail.com>
2024-07-18 13:04:53 +0900

    fix7

--- spec/system/groups/group_spec.rb
+++ spec/system/groups/group_spec.rb
@@ -1,14 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe 'グループ管理', type: :system do
-  let!(:organization) { create(:organization) }
-  let!(:system_admin) { create(:system_admin, confirmed_at: Time.now) }
-  let!(:user_staff) { create(:user_staff, confirmed_at: Time.now) }
-  let!(:user_owner) { create(:user_owner, confirmed_at: Time.now) }
-  let!(:viewer) { create(:viewer) }
-  let!(:viewer1) { create(:viewer1) }
-  let!(:organization_viewer) { create(:organization_viewer) }
-  let!(:organization_viewer2) { create(:organization_viewer2) }
+  let(:organization) { create(:organization) }
+  let(:system_admin) { create(:system_admin, confirmed_at: Time.now) }
+  let(:user_staff) { create(:user_staff, confirmed_at: Time.now) }
+  let(:user_owner) { create(:user_owner, confirmed_at: Time.now) }
+  let(:viewer) { create(:viewer) }
+  let(:viewer1) { create(:viewer1) }
+  let(:organization_viewer) { create(:organization_viewer) }
+  let(:organization_viewer2) { create(:organization_viewer2) }
+
+  before(:each) do
+    organization
+    system_admin
+    user_staff
+    user_owner
+    viewer
+    viewer1
+    organization_viewer
+    organization_viewer2
+  end
 
   describe '正常系' do
     describe 'グループの新規登録' do
@@ -16,10 +27,12 @@ RSpec.describe 'グループ管理', type: :system do
 
       before(:each) do
         sign_in(current_user)
-        visit new_group_path
+        visit groups_path
       end
 
-      it '正常系: 視聴グループが正常に作成される' do
+      it '視聴グループが正常に作成される' do
+        expect(page).to have_content('視聴グループ　新規作成画面へ')
+        visit new_group_path
         fill_in 'group[name]', with: 'Group Name'
         expect(page).to have_select('viewer-select', options: [organization_viewer.viewer.name, organization_viewer2.viewer.name])
         select organization_viewer.viewer.name, from: 'viewer-select'
@@ -38,7 +51,9 @@ RSpec.describe 'グループ管理', type: :system do
           visit groups_path
         end
 
-        it '正常系: 正しい情報を入力すればグループの編集ができて一覧画面に移動する' do
+        it '正しい情報を入力すればグループの編集ができて一覧画面に移動する' do
+          expect(page).to have_content('New Group Name')
+          expect(page).to have_content('編集')
           group = Group.find_by(name: 'New Group Name')
           visit edit_group_path(group.uuid)
           fill_in 'group[name]', with: 'Edited Group Name'
@@ -53,10 +68,10 @@ RSpec.describe 'グループ管理', type: :system do
 
         before(:each) do
           sign_in(system_admin)
-          visit edit_group_path(group.uuid)
         end
 
-        it '正常系: 正しい情報を入力すればグループの編集ができて一覧画面に移動する' do
+        it '正しい情報を入力すればグループの編集ができて一覧画面に移動する' do
+          visit edit_group_path(group.uuid)
           fill_in 'group[name]', with: 'Edited Group Name'
           find('input[name="commit"]').click
           expect(page).to have_current_path groups_path, ignore_query: true
@@ -73,7 +88,7 @@ RSpec.describe 'グループ管理', type: :system do
         visit groups_path
       end
 
-      it '正常系: 視聴グループの削除に成功する' do
+      it '視聴グループの削除に成功する' do
         expect(page).to have_content(group.name)
         find_link('削除', href: group_path(group.uuid)).click
         page.driver.browser.switch_to.alert.accept
@@ -90,13 +105,14 @@ RSpec.describe 'グループ管理', type: :system do
 
       before(:each) do
         sign_in(current_user)
-        visit new_group_path
+        visit groups_path
       end
 
-      it '異常系: 視聴グループ名が空の場合、エラーメッセージが表示される' do
+      it '視聴グループ名が空の場合、エラーメッセージが表示される' do
+        visit new_group_path
         fill_in 'group[name]', with: ''
         find('input[name="commit"]').click
-        expect(page).to have_current_path group_path
+        expect(page).to have_current_path groups_path
         expect(page).to have_content('視聴グループ名を入力してください')
       end
     end
@@ -111,12 +127,12 @@ RSpec.describe 'グループ管理', type: :system do
           visit groups_path
         end
 
-        it '異常系: グループ名を空で更新しようとするとエラーメッセージが表示される' do
+        it 'グループ名を空で更新しようとするとエラーメッセージが表示される' do
           group = Group.find_by(name: 'New Group Name')
           visit edit_group_path(group.uuid)
           fill_in 'group[name]', with: ''
           find('input[name="commit"]').click
-          expect(page).to have_current_path edit_group_path(group.uuid, organization_id: group.organization_id)
+          expect(page).to have_current_path(edit_group_path(group.uuid, organization_id: group.organization_id))
           expect(page).to have_content('視聴グループ名を入力してください')
         end
       end
@@ -126,13 +142,13 @@ RSpec.describe 'グループ管理', type: :system do
 
         before(:each) do
           sign_in(system_admin)
-          visit edit_group_path(group.uuid)
         end
 
-        it '異常系: グループ名を空で更新しようとするとエラーメッセージが表示される' do
+        it 'グループ名を空で更新しようとするとエラーメッセージが表示される' do
+          visit edit_group_path(group.uuid)
           fill_in 'group[name]', with: ''
           find('input[name="commit"]').click
-          expect(page).to have_current_path edit_group_path(group.uuid, organization_id: group.organization_id)
+          expect(page).to have_current_path(edit_group_path(group.uuid, organization_id: group.organization_id))
           expect(page).to have_content('視聴グループ名を入力してください')
         end
       end
@@ -146,7 +162,7 @@ RSpec.describe 'グループ管理', type: :system do
         visit groups_path
       end
 
-      it '異常系: 投稿者でログインしている場合、視聴グループの削除に失敗する' do
+      it '投稿者でログインしている場合、視聴グループの削除に失敗する' do
         expect(page).to have_content(group.name)
         find_link('削除', href: group_path(group.uuid)).click
         page.driver.browser.switch_to.alert.accept
