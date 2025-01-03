05f3af590e464d7236fae8ccaad0b9a7d83f4aed
Takahiro <sample@email.com>
2025-01-03 16:07:43 +0900

    動画投稿ページ作成

--- app/controllers/aws_s3_controller.rb
+++ app/controllers/aws_s3_controller.rb
@@ -1,7 +1,24 @@
 class AwsS3Controller < ApplicationController
   def new
+    @aws = AwsS3.new
+  end
+
+  def create
+    @aws = AwsS3.(newaws_s3_params)
+    if @aws.save
+      redirect_to aws_s3_path(@aws), notice: '動画が正常にアップロードされました。'
+    else
+      render :new
+    end
   end
 
   def index
+    @aws = AwsS3.all
+  end
+
+
+  private
+  def aws_s3_params
+    params.require(:aws_s3).permit(:title, :file_url, :thumbnail_url, :status)
   end
 end
